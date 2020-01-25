a4cb8a0142328016891956872fd1d0302042bbb1
Frank McAuley <frank.mcauley@gmail.com>
2020-01-25 10:36:58 -0500

    FM - examples of how to not use conditionals in test

--- UnitTestPracticeTests/UnitTestPracticeTests.swift
+++ UnitTestPracticeTests/UnitTestPracticeTests.swift
@@ -18,4 +18,22 @@ class UnitTestPracticeTests: XCTestCase {
     func test_fail_withSimpleMessage() {
         XCTFail("We have an issue!")
     }
+    
+    func test_fail_withStringInterpolationMessage() {
+        let theAnswer = 42
+        XCTFail("The answer to the question is: \(theAnswer)")
+    }
+    
+    //bad : example of conditional logic in a test, this needs to be avoided
+    func test_avoidConditionalCode() {
+        let success = false
+        if !success {
+            XCTFail()
+        }
+    }
+    
+    func test_assertTrue() {
+        let success = false
+        XCTAssertTrue(success)
+    }
 }
