b2f3dc93c722ba770302ade080ebefea6c9b6764
Frank McAuley <frank.mcauley@gmail.com>
2020-01-29 12:14:25 -0500

    FM - Adding code to this from the Algorithms Book

--- UnitTestPracticeTests/UnitTestPracticeTests.swift
+++ UnitTestPracticeTests/UnitTestPracticeTests.swift
@@ -67,4 +67,69 @@ class UnitTestPracticeTests: XCTestCase {
         let optionalValue: StructWithDescription = StructWithDescription(x: 1, y: 2)
         XCTAssertNil(optionalValue)
     }
+    
+    
+    // Testing for Equality
+    func test_assertEqual() {
+        let actual = "actual"
+        XCTAssertEqual(actual, "expected")
+    }
 }
+
+
+var numberList: Array<Int> = [1,2,3,4,5,6,7,8,9,10]
+
+// Linear Time vs Logarithmic
+
+// Linear
+/*
+    While this approch achives our goal, each item contained in the set must be evaludated. A function like this is
+    said to run in linear time because its speed is dependent on its input size. In other words, the algorithm becomes
+    less efficient as its input size (n) grows
+ */
+extension Array where Element: Comparable {
+    func linearSearch(forElement key: Element) -> Bool {
+        //check all possible values
+        for number in self {
+            if number == key {
+                return true
+            }
+        }
+        
+        return false
+    }
+}
+
+// Binary Search Logarithmic
+extension Array where Element: Comparable {
+    mutating func binarySearch(forElement key: Element) -> Bool {
+        var result = false
+        
+        //establish indices
+        let min = self.startIndex
+        let max = self.endIndex - 1
+        let mid = startIndex + (self.count/2)
+       
+        // check bounds
+        if key > self[max] || key < self[min] {
+            print("search value \(key) not found")
+            return false
+        }
+        
+        let n = self[mid]
+        
+        if n > key {
+            var slice = Array(self[min...mid - 1])
+            result = slice.binarySearch(forElement: key)
+        }
+        else if n < key {
+            var slice = Array(self[mid + 1...max])
+            result = slice.binarySearch(forElement: key)
+        }
+        else {
+            result = true
+        }
+        return result
+    }
+}
+
